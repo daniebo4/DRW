270393412fe89fd313171e1aa369e70223b03bb7
Lior Abergel <91056461+LiorAbergel@users.noreply.github.com>
2022-12-11 13:10:28 +0200

    Delete Student.py

--- Student.py
+++
@@ -1,6 +0,0 @@
-class Student:
-    def __init__(self, username, password, ID, secret_word):
-        self.username = username
-        self.password = password
-        self.ID = ID
-        self.secret_word = secret_word.replace("\n", "")
