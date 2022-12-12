2f4fe788b9b4fe72a4efc4b9c10e777f232b0849
maormer12 <117373045+maormer12@users.noreply.github.com>
2022-12-12 16:13:13 +0200

    Revert "Update main.py"
    This reverts commit 9a50de688ca0754231088b4d3b536c27d8624519.

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import registerLayout
 
 db = Data_base('Students_data.txt', 'Workers_data.txt')
 #test
-#tes2
 
 def check_login_student(ID, Password):
     """a function that checks if student`s ID and password are matching"""
