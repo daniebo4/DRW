9a50de688ca0754231088b4d3b536c27d8624519
maormer12 <117373045+maormer12@users.noreply.github.com>
2022-12-12 16:11:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import registerLayout
 
 db = Data_base('Students_data.txt', 'Workers_data.txt')
 #test
+#tes2
 
 def check_login_student(ID, Password):
     """a function that checks if student`s ID and password are matching"""
