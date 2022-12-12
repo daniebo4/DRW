083c2137c3a9a251c8fd24069d58b1d22638a828
daniebo4 <daniebo4@ac.sce.ac.il>
2022-12-12 20:19:20 +0200

    Create unit testing for register layout

---
+++ registerLayout_testing.py
@@ -0,0 +1,13 @@
+import unittest
+import self as self
+import registerLayout
+
+
+class registerTest(unittest.TestCase):
+    def setUp(self):
+
+
+    def test_userName(self):
+        self.assertIsNone(x)
+
+
