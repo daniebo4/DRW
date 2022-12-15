fb004368cfd960d8273a54b7bc92b036a57c03a5
eladlavi55 <117382337+eladlavi55@users.noreply.github.com>
2022-12-15 15:51:13 +0200

    Add files via upload
    YOLO

--- Layouts/StudentMenuItemsLayoutDraft.py
+++ Layouts/StudentMenuItemsLayoutDraft.py
@@ -1,20 +1,24 @@
 import PySimpleGUI as sg
 
 Info = [
-    ['Eraser', '10', '1914', '1919', 'Very nice and handy', '$$$$'],
-    ['Three Hole Punch', '5', '1965', '2025', 'Not enough holes', '$$$$$'],
-    ['Fine Brush', '2', '1988', '2001', 'Too Fine', '$$'],
-    ['Color Pallet', '5', '2002', '2002', 'Lots of colors', '$$$'],
+    ['Eraser', '10', '1914', '1919','Very nice and handy','$$$$'],
+    ['Three Hole Punch', '5', '1965', '2025','Not enough holes','$$$$$'],
+    ['Fine Brush', '2', '1988', '2001','Too Fine','$$'],
+    ['Color Pallete', '5', '2002', '2002','Lots of colors','$$$'],
 ]
 
-headings = ['Item', 'Quantity', 'Loan Date', 'Due Date', 'Description', 'Rating']
+My_Items = [
+    ['Gucci', '5', '69', '420','OG','$$$$$'],
+    ['Shavtoosha', '1', '69', '420','Pay attention,please','$$$$$'],
+]
 
+headings = ['Item', 'Quantity', 'Loan Date','Due Date','Description','Rating']
 
 def open_student_window():
     layout = [
         [sg.Table(values=Info,
                   headings=headings,
-                  max_col_width=35,
+                  max_col_width=25,
                   auto_size_columns=True,
                   display_row_numbers=False,
                   justification='l',
@@ -22,8 +26,8 @@ def open_student_window():
                   key='-TABLE-',
                   row_height=35)],
         [sg.Button('Request Item', size=(15, 1)),
-         sg.Button('My Items', size=(15, 1)),
-         sg.Exit(pad=((430, 0), (0, 0)))]
+        sg.Button('My Items', size=(15, 1)),
+        sg.Exit(pad=((430, 0), (0, 0)))]
 
     ]
 
@@ -34,9 +38,53 @@ def open_student_window():
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
         if event == "Request Item" or event == sg.WIN_CLOSED:
+            Request_item_Window()
             break
         if event == "My Items" or event == sg.WIN_CLOSED:
+            My_Items_window()
+
+
+def My_Items_window():
+    layout = [
+        [sg.Table(values=My_Items,
+                  headings=headings,
+                  max_col_width=25,
+                  auto_size_columns=True,
+                  display_row_numbers=False,
+                  justification='l',
+                  num_rows=10,
+                  key='-TABLE-',
+                  row_height=35)],
+        [sg.Button('Return', size=(15, 1)),
+        sg.Exit(pad=((430, 0), (0, 0)))]
+
+    ]
+
+    window = sg.Window("My Items", layout)
+
+    while True:
+        event, values = window.read()
+        if event == "Return" or event == sg.WIN_CLOSED:
+            break
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            My_Items_window().close
             break
 
+def Request_item_Window():
+    layout = [
+        [sg.Text("Are you Sure?")],
+         sg.Button('Yes', ),
+          sg.Button('No', ),
+    ]
+
+    window = sg.Window("Request Item", layout)
+
+    while True:
+        event, values = window.read()
+        if event == "Yes" or event == sg.WIN_CLOSED:
+            break
+        if event == "No" or event == sg.WIN_CLOSED:
+            Request_item_Window.close
+            break
 
 open_student_window()
