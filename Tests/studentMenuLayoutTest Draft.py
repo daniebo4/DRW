b6af21c2ddf660ac585f96d6355ead41ad920541
eladlavi55 <117382337+eladlavi55@users.noreply.github.com>
2022-12-16 01:21:36 +0200

    Add files via upload
    YOLO

---
+++ Tests/studentMenuLayoutTest Draft.py
@@ -0,0 +1,24 @@
+import unittest
+from Layouts import forgotPasswordLayout
+import main
+
+
+class studentMenuLayout(unittest.TestCase):
+
+    def test1_return_item(self):
+        # Tests if status of returned item is changed to "pending"
+        main.studentMenuLayout.open_my_items_window(main.db.student_dict[2])
+        self.assertEqual(main.db.item_dict[10].status, 'pending')
+
+    def test2_Request_item(self):
+        # Tests if status of returned item is changed to "pending"
+        main.studentMenuLayout.open_request_item_window(main.db.student_dict[1],main.db.item_dict[10])
+        self.assertEqual(main.db.item_dict[10].owner, 'pending')
+
+    def test3_Rating_item(self):
+        # draft
+        main.studentMenuLayout.rate(main.db.student_dict[1] ,5, main.db.item_dict[10])
+        self.assertEqual(((main.student_loaned_items[main.db.item_dict[10]].rating * (main.student_loaned_items[main.db.item_dict[10]].num_raters-1))+5)/(main.student_loaned_items[main.db.item_dict[10]].num_raters+1))
+
+if __name__ == '__main__':
+    unittest.main()
