714894e44d4947255a65c8153a67a782ffafd3af
maormer12 <117373045+maormer12@users.noreply.github.com>
2022-12-16 14:04:54 +0200

    write commits and tesr units fix

--- Tests/studentMenuLayout_test.py
+++ Tests/studentMenuLayout_test.py
@@ -2,7 +2,7 @@ import unittest
 import main
 
 
-class studentMenuLayout(unittest.TestCase):
+class studentMenuTest(unittest.TestCase):
     def test1_rating_item(self):
         """tries to rate an item twice to see if it does avg and updates"""
         self.assertEqual(main.studentMenuLayout.rate(5, 'Item test'), '5.0')
@@ -19,6 +19,9 @@ class studentMenuLayout(unittest.TestCase):
         """tries to return item and checks if request item function succeeds and returns True"""
         self.assertFalse(main.studentMenuLayout.return_item([], '1'))
 
+    def test4_studentLayout(self):
+        """tries to check if the window shows the item"""
+        self.assertFalse(main.studentMenuLayout.return_item([], '1'))
 
 if __name__ == '__main__':
     unittest.main()
